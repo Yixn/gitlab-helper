c3e6c41c560282539ba5343e64a8b1ffc24e243f
Daniel Samer <daniel@linkster.co>
2025-04-22 17:44:09 +0200

    automated push

--- build.js
+++ build.js
@@ -11,7 +11,7 @@ const { minify } = require('terser');
 const glob = require('glob');
 
 // Define version number in a single place
-const VERSION = '1.6';
+const VERSION = '1.7';
 
 // Load .env file if it exists
 let envConfig = {};
